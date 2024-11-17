0913971d035520de5029952de9cf4ee21a45f061
Ju GyeongMin <leseul04@naver.com>
2024-11-17 22:52:36 +0900

    Add geo package with init, utils, and tester files

---
+++ tester.py
@@ -0,0 +1,11 @@
+from geo.utils import pythagoras, circle
+
+# calculate the length of hypotenuse(c) when a=3 and b=4.
+a, b = 3, 4
+c = pythagoras(a, b)
+print('c =', c)
+
+# calculate the area of circle with radius r=10
+r = 10
+area = circle(r)
+print('area =', area)
